30248eb0143c556390fa39fc7972f459017b28dc
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-21 00:00:34 +0530

    Refactor code to remove unused imports

--- Threads-Frontend/src/pages/CropForm.jsx
+++ Threads-Frontend/src/pages/CropForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom"; // For navigation
