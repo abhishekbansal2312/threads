5d17c72c1a64c0d35317e81aa709bffe400258de
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-21 00:11:15 +0530

    Refactor code to add CORS middleware and remove unused imports

--- Threads-Backend/index.js
+++ Threads-Backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Import cors
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -24,6 +25,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// CORS middleware - add this before other middlewares
+app.use(
+  cors({
+    origin: true, // Allows any origin
+    credentials: true, // Allows cookies to be sent with requests
+  })
+);
+
 // Middlewares
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
@@ -33,8 +42,6 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
-
-// http://localhost:5000 => backend,frontend
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
